2c6597943b4f456888573f197b4779f61fcfce5f
ing-jose-rafael <ing.jcarreno@gmail.com>
2023-09-12 00:03:22 -0500

    layout comun entre rutas no anidadas

---
+++ src/app/(with-banner)/layout.jsx
@@ -0,0 +1,8 @@
+export default function layout ({ children }) {
+  return (
+    <>
+      <marquee className='text-white'>El mejor canal de Twitch de la historia</marquee>
+      {children}
+    </>
+  )
+}
