c89bb3f1364e66c37151f0aa81890de4486b5be2
ing-jose-rafael <ing.jcarreno@gmail.com>
2023-09-11 23:55:13 -0500

    stilos en seccion de los comentarios

--- src/app/posts/[id]/comments/page.jsx
+++ src/app/posts/[id]/comments/page.jsx
@@ -1,4 +1,5 @@
-const fetchPostComments = (id) => {
+const fetchPostComments = async (id) => {
+  await new Promise(resolve => setTimeout(resolve, 3000)) // <-- simulate network latency
   // getStaticProps
   return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
     .then(response => response.json())
@@ -8,7 +9,7 @@ export default async function Comments ({ params }) {
   const { id } = params
   const comments = await fetchPostComments(id)
   return (
-    <ul>
+    <ul className='bg-slate-500 p-2'>
       {comments.map(comment => (
         <li key={comment.id} className='mb-4 pl-5'>
           <h2 className='font-bold'>{comment.name}</h2>
